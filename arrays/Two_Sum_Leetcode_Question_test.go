5177fe9a1dc47520a2ba0395c28fb68f71f5e236
arifinlenna <arifin@lenna.ai>
2024-08-14 20:29:45 +0700

    update logic twoSum

--- arrays/Two_Sum_Leetcode_Question_test.go
+++ arrays/Two_Sum_Leetcode_Question_test.go
@@ -12,16 +12,21 @@ import (
 
 func twoSum(nums []int, target int) []int {
 	var resultNums []int
+	// nums: []int{3,2,4},
+	// 		target: 6,
 
 	for i := 0; i < len(nums)-1; i++ {
-		if nums[i] + nums[i+1] == target {
-			resultNums = []int{
-				i,i+1,
+		for j := i+1; j < len(nums); j++ {
+			if nums[i] + nums[j] == target {
+				resultNums = []int{
+					i,j,
+				}
 			}
 		}
 	}
 	return resultNums
 }
+
 
 func TestMain(t *testing.T)  {
 	// var nums = []int{3,2,4}
@@ -48,6 +53,30 @@ func TestMain(t *testing.T)  {
 		{"Example 3", argumentParams{
 			nums: []int{3,3},
 			target: 6,
+		}, []int{0,1}},
+		{"Example 4", argumentParams{
+			nums: []int{1, 3, 7, 9, 2},
+			target: 11,
+		}, []int{3,4}},
+		{"Example 5", argumentParams{
+			nums: []int{1, 3, 7, 9, 2},
+			target: 25,
+		}, []int(nil)},
+		{"Example 6", argumentParams{
+			nums: []int{3,2,3},
+			target: 6,
+		}, []int{0,2}},
+		{"Example 7", argumentParams{
+			nums: []int{},
+			target: 1,
+		}, []int(nil)},
+		{"Example 8", argumentParams{
+			nums: []int{5},
+			target: 5,
+		}, []int(nil)},
+		{"Example 9", argumentParams{
+			nums: []int{1,6},
+			target: 7,
 		}, []int{0,1}},
 	}
 
